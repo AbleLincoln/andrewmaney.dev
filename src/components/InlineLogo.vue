0578028cc26b6e048fc865fd27763b86454ccfce
AbleLincoln <maneyandrew@gmail.com>
2023-04-27 17:00:00 -0700

    that space was annoying me

--- src/components/InlineLogo.vue
+++ src/components/InlineLogo.vue
@@ -11,8 +11,10 @@ const { icon, color } = BRANDS[props.text]
 
 <template>
   <span :style="{ color: fullColor ? color : '' }" class="ml-1 whitespace-nowrap">
-    <font-awesome-icon :icon="['fab', icon]" :style="{ color }" class="text-xl" />
-    <b class="hidden sm:inline">{{ text }}</b>
+    <font-awesome-icon :icon="['fab', icon]" :style="{ color }" class="text-xl" /><b
+      class="hidden sm:inline"
+      >{{ text }}</b
+    >
   </span>
 </template>
 
